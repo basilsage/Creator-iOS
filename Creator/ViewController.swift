d951661baca95ebbdfcff5ab3c3e4c34361204aa
DJ Satoda <dj@DJs-MacBook-Pro.local>
2020-04-25 12:57:18 -0700

    Initial Commit

---
+++ Creator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Creator
+//
+//  Created by DJ Satoda on 4/25/20.
+//  Copyright © 2020 DJ Satoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
