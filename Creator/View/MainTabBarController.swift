b0a5565e0976f1a7a9c81ba52504c3a5dbbbbaac
DJ Satoda <dj@DJs-MacBook-Pro.local>
2020-07-10 23:15:50 -0700

    Committing again, converting demo UI to MVP UI

--- Creator/View/MainTabBarController.swift
+++ Creator/View/MainTabBarController.swift
@@ -11,7 +11,7 @@ import UIKit
 import Firebase
 
 class MainTabBarController: UITabBarController, UITabBarControllerDelegate {
-    
+     
     override func viewDidLoad() {
         super.viewDidLoad()
         
